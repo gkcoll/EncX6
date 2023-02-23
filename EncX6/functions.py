fcbed1986d27563057c716bcf8c66ca6bf5629a9
Albert Lin <110967456+gkcoll@users.noreply.github.com>
2023-02-23 23:33:39 +0800

    Update functions.py

--- EncX6/functions.py
+++ EncX6/functions.py
@@ -1,23 +1,32 @@
+'''
+@File    :   functions.py
+@Time    :   2023/02/12 20:00:54
+@Author  :   @灰尘疾客
+@Version :   1.0
+@Site    :   https://www.gkcoll.xyz
+@Desc    :   Some common functions.
+'''
+
+
 import time
 
 
-def keeplog(content: str, log: bool = True) -> None:
-    if log:
-        date = time.strftime("%Y%m%d")
+def keeplog(content: str) -> None:
+    date = time.strftime("%Y%m%d")
+    try:
         with open("usedlog\\" + date + ".log", "a", encoding="utf-8") as f:
             f.write(content + "\n")
-    else:
+    except FileNotFoundError:
         pass
 
 
 def cut(obj: str | list, sec: int) -> list:
-    """实现字符串/列表按步平均切割。"""
+    """Cut str or list by step."""
     return [obj[i:i+sec] for i in range(0, len(obj), sec)]
 
 
 def debug_print(content: str, debug: bool = False, log: bool = True) -> None:
     if debug:
         print("=================================\n"+content)
-    else:
-        pass
-    keeplog(content, log)
+    else:pass
+    keeplog(content)
