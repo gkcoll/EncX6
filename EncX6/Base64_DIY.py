74d9a240f9156989dadf1a5f2e584793f365e9ca
Albert Lin <110967456+gkcoll@users.noreply.github.com>
2023-02-23 23:32:05 +0800

    Update Base64_DIY.py

--- EncX6/Base64_DIY.py
+++ EncX6/Base64_DIY.py
@@ -1,29 +1,39 @@
+'''
+@File    :   Base64_DIY.py
+@Time    :   2023/02/12 18:39:02
+@Author  :   @灰尘疾客
+@Version :   1.0
+@Site    :   https://www.gkcoll.xyz
+@Desc    :   A customized base64 module.
+'''
+
+
 from base64 import b64encode, b64decode
 from EncX6.functions import debug_print as dbp
 anti_counterfeit = ['gk', 'coll']
 
 
 def encode(content: str = "Test") -> str:
-    """实现 Base64 编码"""
-    content_x = content.join(anti_counterfeit)  # 加防伪
-    # 编码
+    """Base64 encode function."""
+    content_x = content.join(anti_counterfeit)  # Add anti-counterfeit identification
+    # Encoding
     try:
         encoded = str(b64encode(content_x.encode('utf-8'))).strip("b'")
     except:
-        dbp("Base64 编码错误")
+        dbp("Base64 encoding error!")
     else:
-        dbp("Base64加密结果:" + encoded)
+        dbp("Base64 encoding error!: " + encoded)
         return encoded
 
 
 def decode(encoded: str) -> str:
-    """用以解码Base64编码内容。"""
+    """Base64 decode function."""
     try:
         decoded = b64decode(encoded).decode("utf-8")
     except:
-        dbp("Base64 解码错误")
+        dbp("Base64 decoding error!")
     else:
         result = decoded.lstrip(
             anti_counterfeit[0]).rstrip(anti_counterfeit[1])
-        dbp("Base64解密结果:" + result)
+        dbp("Base64 decoding error: " + result)
         return result
